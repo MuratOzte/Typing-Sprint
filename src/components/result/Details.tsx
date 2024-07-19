b6292bf3908565540514f0ee57af26bb7fd97aad
MuratOzte <mo2553481@gmail.com>
2024-07-19 09:26:42 +0300

    refactor: Update Details page layout and add share buttons

--- src/components/result/Details.tsx
+++ src/components/result/Details.tsx
@@ -7,7 +7,7 @@ import { FaLink } from 'react-icons/fa';
 const Details = () => {
     return (
         <div className="w-10/12 flex justify-center flex-col gap-5">
-            <div className="flex items-center px-8 py-2 gap-3 bg-slate-600 rounded-md mt-16 ">
+            <div className="flex items-center px-8 py-2 gap-8 bg-slate-600 rounded-md mt-16 ">
                 <IoTrophy className="text-9xl text-yellow-500" size={48} />
                 <p className="text-base text-slate-200 flex flex-col">
                     <span>You Beat</span>
@@ -15,19 +15,22 @@ const Details = () => {
                     <span>of the world</span>
                 </p>
             </div>
-            <div className="flex justify-evenly">
-                <button className="hover:scale-110 transition-all duration-200">
-                    <FaWhatsapp size={24} className="text-green-500" />
-                </button>
-                <button className="hover:scale-110 transition-all duration-200">
-                    <FaInstagram size={24} className="text-red-500" />
-                </button>
-                <button className="hover:scale-110 transition-all duration-200">
-                    <FaFacebook size={24} className="text-blue-500" />
-                </button>
-                <button className="hover:scale-110 transition-all duration-200">
-                    <FaLink size={24} className="text-slate-500" />
-                </button>
+            <div className="bg-slate-600 py-2 pb-3 rounded-md">
+                <p className="text-lg mb-2 text-slate-200">Share with</p>
+                <div className="flex justify-evenly">
+                    <button className="hover:scale-110 transition-all duration-200">
+                        <FaWhatsapp size={24} className="text-green-500" />
+                    </button>
+                    <button className="hover:scale-110 transition-all duration-200">
+                        <FaInstagram size={24} className="text-red-500" />
+                    </button>
+                    <button className="hover:scale-110 transition-all duration-200">
+                        <FaFacebook size={24} className="text-blue-500" />
+                    </button>
+                    <button className="hover:scale-110 transition-all duration-200">
+                        <FaLink size={24} className="text-slate-400" />
+                    </button>
+                </div>
             </div>
         </div>
     );
