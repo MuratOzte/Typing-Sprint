7c162c66ce753cfc03a0d40e6426a9eeb056c29d
MuratOzte <mo2553481@gmail.com>
2024-08-15 05:42:46 +0300

    run slice added

---
+++ src/store/slices/runSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface run {}
+
+const initialState = {};
+
+const uiSlice = createSlice({
+    name: 'ui',
+    initialState,
+    reducers: {},
+});
+
+export default uiSlice;
