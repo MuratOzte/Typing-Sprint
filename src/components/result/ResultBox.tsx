c1a168a86f3f0f13634bc4ff391da42e8daf3df8
MuratOzte <mo2553481@gmail.com>
2024-08-31 22:00:06 +0300

    chore: Update ResultBox component styles for better UI consistency

--- src/components/result/ResultBox.tsx
+++ src/components/result/ResultBox.tsx
@@ -5,10 +5,10 @@ import WpmBox from './WpmBox';
 
 const ResultBox = () => {
     return (
-        <div className="w-6/12 h-screen relative">
+        <div className="w-6/12 h-screen relative bg-slate-900">
             <Nav />
             <div className="flex justify-center ">
-                <div className="w-5/6 flex text-4xl text-center mt-40 h-72 bg-gray-400 rounded-md shadow-xl">
+                <div className="w-5/6 flex text-4xl text-center mt-40 h-72 bg-slate-700 rounded-md shadow-xl">
                     <div className="flex flex-col w-1/2">
                         <WpmBox />
                         <Statistics />
